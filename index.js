ce169b16e0c38e103f0c1304b4339a9339ddac06
bgzdhn <55092116+bgzdhn@users.noreply.github.com>
2019-09-25 18:10:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './app.js';
+import './style.css';
+
+ReactDOM.render(
+	<Clock />,
+	document.getElementById('root')
+);
